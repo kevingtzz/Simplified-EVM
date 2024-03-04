8087340e0ab025959b5d790eafb7a7d95e184161
Kevin Gutierrez <kevingtzz@outlook.com>
2024-03-04 02:17:25 -0500

    feat: first ruc cycle

--- src/classes/execution/index.ts
+++ src/classes/execution/index.ts
@@ -1,4 +1,4 @@
-import { isHexString } from "@ethersproject/bytes";
+import { isHexString, arrayify, hexlify } from "@ethersproject/bytes";
 
 import Stack from "../stack";
 import Memory from "../memory";
@@ -7,16 +7,51 @@ import { ExecutionError, ExecutionErrorCodes } from "./errors";
 
 class ExecutionContext {
   private readonly code: Uint8Array;
-  public stack: Stack = new Stack();
-  public memory: Memory = new Memory();
   private pc: number;
   private stopped: boolean;
 
+  public stack: Stack;
+  public memory: Memory;
+
   constructor(code: string) {
-    if (!isHexString(code))
+    if (!isHexString(code) || code.length % 2 !== 0) {
       throw new ExecutionError(
         "Invalid bytecode",
         ExecutionErrorCodes.InvalidBytecode
       );
+    }
+
+    this.code = arrayify(code);
+    this.stack = new Stack();
+    this.memory = new Memory();
+    this.pc = 0;
+    this.stopped = false;
+  }
+
+  public stop(): void {
+    this.stopped = true;
+    console.info("Execution context stopped");
+  }
+
+  public run(): void {
+    console.info("Running execution context...");
+    while (!this.stopped) {
+      const opcode = this.readBytesFromCode(1);
+      console.log(
+        `Execution state:\nPC: ${this.pc}\nCurrent opcode: ${opcode}`
+      );
+      console.log("--------------------");
+    }
+  }
+
+  public readBytesFromCode(bytes: number = 1): bigint {
+    const hexValues = this.code.slice(this.pc, this.pc + bytes);
+    const values = BigInt(hexlify(hexValues));
+
+    this.pc += bytes;
+
+    return values;
   }
 }
+
+export default ExecutionContext;
