b944e62b36e683173c0a04d22c9f517d8fba7aad
Kevin Gutierrez <kevingtzz@outlook.com>
2024-03-08 04:25:10 -0500

    feat: aritmetic opcodes & PUSH opcodes

--- src/opcodes/index.ts
+++ src/opcodes/index.ts
@@ -8,20 +8,74 @@ const Opcodes: {
   0x00: new Instruction(0x00, "STOP", (context: ExecutionContext) =>
     context.stop()
   ),
-  0x01: new Instruction(0x01, "ADD"),
-  0x02: new Instruction(0x02, "MUL"),
-  0x03: new Instruction(0x03, "SUB"),
-  0x04: new Instruction(0x04, "DIV"),
-  0x06: new Instruction(0x06, "MOD"),
-  0x0a: new Instruction(0x0a, "EXP"),
-  0x10: new Instruction(0x10, "LT"),
-  0x11: new Instruction(0x11, "GT"),
-  0x14: new Instruction(0x14, "EQ"),
-  0x16: new Instruction(0x16, "AND"),
-  0x17: new Instruction(0x17, "OR"),
-  0x18: new Instruction(0x18, "XOR"),
-  0x19: new Instruction(0x19, "NOT"),
-  0x50: new Instruction(0x50, "POP"),
+  0x01: new Instruction(0x01, "ADD", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a + b;
+    context.stack.push(result);
+  }),
+  0x02: new Instruction(0x02, "MUL", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a * b;
+    context.stack.push(result);
+  }),
+  0x03: new Instruction(0x03, "SUB", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a - b;
+    context.stack.push(result);
+  }),
+  0x04: new Instruction(0x04, "DIV", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a / b;
+    context.stack.push(result);
+  }),
+  0x06: new Instruction(0x06, "MOD", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a % b;
+    context.stack.push(result);
+  }),
+  0x0a: new Instruction(0x0a, "EXP", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = a ** b;
+    context.stack.push(result);
+  }),
+  0x10: new Instruction(0x10, "LT", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a < b);
+    context.stack.push(result);
+  }),
+  0x11: new Instruction(0x11, "GT", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a > b);
+    context.stack.push(result);
+  }),
+  0x14: new Instruction(0x14, "EQ", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a == b);
+    context.stack.push(result);
+  }),
+  0x16: new Instruction(0x16, "AND", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a & b);
+    context.stack.push(result);
+  }),
+  0x17: new Instruction(0x17, "OR", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a | b);
+    context.stack.push(result);
+  }),
+  0x18: new Instruction(0x18, "XOR", (context: ExecutionContext) => {
+    const [a, b] = [context.stack.pop(), context.stack.pop()];
+    const result = BigInt(a ^ b);
+    context.stack.push(result);
+  }),
+  0x19: new Instruction(0x19, "NOT", (context: ExecutionContext) => {
+    const a = context.stack.pop();
+    const result = BigInt(~a);
+    context.stack.push(result);
+  }),
+  0x50: new Instruction(0x50, "POP", (context: ExecutionContext) => {
+    context.stack.pop();
+  }),
   0x51: new Instruction(0x51, "MLOAD"),
   0x52: new Instruction(0x52, "MSTORE"),
   0x54: new Instruction(0x54, "SLOAD"),
@@ -29,38 +83,102 @@ const Opcodes: {
   0x56: new Instruction(0x56, "JUMP"),
   0x57: new Instruction(0x57, "JUMPI"),
   0x5b: new Instruction(0x5b, "JUMPDEST"),
-  0x60: new Instruction(0x60, "PUSH1"),
-  0x61: new Instruction(0x61, "PUSH2"),
-  0x62: new Instruction(0x62, "PUSH3"),
-  0x63: new Instruction(0x63, "PUSH4"),
-  0x64: new Instruction(0x64, "PUSH5"),
-  0x65: new Instruction(0x65, "PUSH6"),
-  0x66: new Instruction(0x66, "PUSH7"),
-  0x67: new Instruction(0x67, "PUSH8"),
-  0x68: new Instruction(0x68, "PUSH9"),
-  0x69: new Instruction(0x69, "PUSH10"),
-  0x6a: new Instruction(0x6a, "PUSH11"),
-  0x6b: new Instruction(0x6b, "PUSH12"),
-  0x6c: new Instruction(0x6c, "PUSH13"),
-  0x6d: new Instruction(0x6d, "PUSH14"),
-  0x6e: new Instruction(0x6e, "PUSH15"),
-  0x6f: new Instruction(0x6f, "PUSH16"),
-  0x70: new Instruction(0x70, "PUSH17"),
-  0x71: new Instruction(0x71, "PUSH18"),
-  0x72: new Instruction(0x72, "PUSH19"),
-  0x73: new Instruction(0x73, "PUSH20"),
-  0x74: new Instruction(0x74, "PUSH21"),
-  0x75: new Instruction(0x75, "PUSH22"),
-  0x76: new Instruction(0x76, "PUSH23"),
-  0x77: new Instruction(0x77, "PUSH24"),
-  0x78: new Instruction(0x78, "PUSH25"),
-  0x79: new Instruction(0x79, "PUSH26"),
-  0x7a: new Instruction(0x7a, "PUSH27"),
-  0x7b: new Instruction(0x7b, "PUSH28"),
-  0x7c: new Instruction(0x7c, "PUSH29"),
-  0x7d: new Instruction(0x7d, "PUSH30"),
-  0x7e: new Instruction(0x7e, "PUSH31"),
-  0x7f: new Instruction(0x7f, "PUSH32"),
+  0x60: new Instruction(0x60, "PUSH1", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(1));
+  }),
+  0x61: new Instruction(0x61, "PUSH2", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(2));
+  }),
+  0x62: new Instruction(0x62, "PUSH3", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(3));
+  }),
+  0x63: new Instruction(0x63, "PUSH4", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(2));
+  }),
+  0x64: new Instruction(0x64, "PUSH5", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(5));
+  }),
+  0x65: new Instruction(0x65, "PUSH6", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(6));
+  }),
+  0x66: new Instruction(0x66, "PUSH7", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(7));
+  }),
+  0x67: new Instruction(0x67, "PUSH8", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(8));
+  }),
+  0x68: new Instruction(0x68, "PUSH9", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(9));
+  }),
+  0x69: new Instruction(0x69, "PUSH10", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(10));
+  }),
+  0x6a: new Instruction(0x6a, "PUSH11", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(11));
+  }),
+  0x6b: new Instruction(0x6b, "PUSH12", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(12));
+  }),
+  0x6c: new Instruction(0x6c, "PUSH13", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(13));
+  }),
+  0x6d: new Instruction(0x6d, "PUSH14", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(14));
+  }),
+  0x6e: new Instruction(0x6e, "PUSH15", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(15));
+  }),
+  0x6f: new Instruction(0x6f, "PUSH16", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(16));
+  }),
+  0x70: new Instruction(0x70, "PUSH17", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(17));
+  }),
+  0x71: new Instruction(0x71, "PUSH18", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(18));
+  }),
+  0x72: new Instruction(0x72, "PUSH19", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(19));
+  }),
+  0x73: new Instruction(0x73, "PUSH20", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(20));
+  }),
+  0x74: new Instruction(0x74, "PUSH21", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(21));
+  }),
+  0x75: new Instruction(0x75, "PUSH22", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(22));
+  }),
+  0x76: new Instruction(0x76, "PUSH23", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(23));
+  }),
+  0x77: new Instruction(0x77, "PUSH24", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(24));
+  }),
+  0x78: new Instruction(0x78, "PUSH25", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(25));
+  }),
+  0x79: new Instruction(0x79, "PUSH26", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(26));
+  }),
+  0x7a: new Instruction(0x7a, "PUSH27", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(27));
+  }),
+  0x7b: new Instruction(0x7b, "PUSH28", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(28));
+  }),
+  0x7c: new Instruction(0x7c, "PUSH29", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(29));
+  }),
+  0x7d: new Instruction(0x7d, "PUSH30", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(30));
+  }),
+  0x7e: new Instruction(0x7e, "PUSH31", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(31));
+  }),
+  0x7f: new Instruction(0x7f, "PUSH32", (context: ExecutionContext) => {
+    context.stack.push(context.readBytesFromCode(32));
+  }),
   0x80: new Instruction(0x80, "DUP1"),
   0x81: new Instruction(0x81, "DUP2"),
   0x82: new Instruction(0x82, "DUP3"),
